d79d07e5975af91eec150c67e5a094152e94079a
vicky <srivicky2000@gmail.com>
2021-06-11 03:47:27 +0530

    Added UI lib and firebase

--- cwtchadminapp/src/Routes.js
+++ cwtchadminapp/src/Routes.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { View, Text } from '@material-ui/icons';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+
+// Admin Screen
+    import Home from './admin/screens/Home';
+    import Login from "./admin/auth/Login"
+
 const Routes = () => {
     return (
         <BrowserRouter>
         
             <Switch>
-                <Route/>
+                <Route exact path="/" component={Home}/>  
+                <Route exact path="/login" component={Login}/>
+
             </Switch>
         </BrowserRouter>
     )
