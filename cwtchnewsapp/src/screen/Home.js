30cb81bae27eebea2209fd37fd86d80d6af00a23
vicky <srivicky2000@gmail.com>
2021-06-12 14:13:51 +0530

    Done Missing navigation

--- cwtchnewsapp/src/screen/Home.js
+++ cwtchnewsapp/src/screen/Home.js
@@ -1,5 +1,6 @@
 import React,{useEffect,useState} from 'react';
 import { connect} from 'react-redux'
+import { Appbar } from 'react-native-paper';
 import propTypes from 'prop-types'
 import {
   SafeAreaView,
@@ -18,10 +19,15 @@ import {
   } from '@react-native-google-signin/google-signin';
 
 import {googleSignout} from '../action/auth'
+import Settings from './Settings';
 
 
+const Home = ({googleSignout,navigation}) => {
+  const _goBack = () => console.log('Went back');
 
-const Home = ({googleSignout}) => {
+  const _handleSearch = () => console.log('Searching');
+
+  const _handleMore = () => navigation.navigate("Settings");
     useEffect(() => {
 
         GoogleSignin.configure({
@@ -45,18 +51,29 @@ const Home = ({googleSignout}) => {
       };
     return(
         <View>
-            <Text>
-                Home Page Logout
-            </Text>
+           <Appbar.Header
+           style={{backgroundColor:'#EB4D4B'}}
+           >
 
-            <GoogleSigninButton onPress={() => googleSignout()}/>
+      <Appbar.Content title="Title"/>
+      <Appbar.Action icon="magnify" onPress={_handleSearch} />
+      <Appbar.Action icon="cog-outline" onPress={_handleMore} />
+    </Appbar.Header>
+
+      <View>
+        
+      </View>
+
+
+
+            {/* <GoogleSigninButton onPress={() => googleSignout()}/>
             <View>
               <TouchableOpacity style={{backgroundColor:'blue'}}>
                 <Text style={{color:'white',padding:30}}>
                   Phone Signout
                 </Text>
               </TouchableOpacity>
-            </View>
+            </View> */}
         </View>
     )
 }
--- cwtchnewsapp/src/screen/Navigation/HomeMenuNavigation.js
+++ cwtchnewsapp/src/screen/Navigation/HomeMenuNavigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, View, } from 'react-native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from '../Home';
 import Discover from "../Discover";
@@ -11,51 +11,18 @@ import Settings from "../Settings";
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Tab = createMaterialBottomTabNavigator();
-
+const Stack = createStackNavigator();
 const HomeMenuNavigation = () => {
     return(
         <>
-    <Tab.Navigator
-    backBehavior={Discover}
-    tabBarOptions={{
-      activeTintColor: '#CA3E47',
-    }}
-    activeColor="#fff"
-  barStyle={{ backgroundColor: '#EB4D4B' }}
-    
-    >
-      <Tab.Screen name="Home" component={Home} 
-         options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="heart" color={color} size={26} />
-            ),}}
-      />
-      <Tab.Screen name="Discover" component={Discover} 
-       options={{
-        tabBarLabel: 'Discover',
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="library" color={color} size={26} />
-        ),}}
-      
-      />
-      <Tab.Screen name="Article" component={Article} 
-       options={{
-        tabBarLabel: 'Article',
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="box" color={color} size={26} />
-        ),}}
-      />
-        <Tab.Screen name="Settings" component={Settings} 
-       options={{
-        tabBarLabel: 'Setting',
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="cog-outline" color={color} size={26} />
-        ),}}
-      />
-
-    </Tab.Navigator>
+      <Stack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+      >
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Settings" component={Settings} />
+    </Stack.Navigator>
         </>
     )
 
