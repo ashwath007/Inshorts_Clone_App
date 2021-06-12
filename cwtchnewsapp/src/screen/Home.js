7e666d794fb4807bb5b41646eae08f9b9710f2fd
vicky <srivicky2000@gmail.com>
2021-06-13 05:22:28 +0530

    HOME page - main slider added

--- cwtchnewsapp/src/screen/Home.js
+++ cwtchnewsapp/src/screen/Home.js
@@ -1,7 +1,8 @@
 import React,{useEffect,useState} from 'react';
 import { connect} from 'react-redux'
-import { Appbar } from 'react-native-paper';
+import { Appbar,Searchbar,Subheading   } from 'react-native-paper';
 import propTypes from 'prop-types'
+import Carousel from 'react-native-snap-carousel';
 import {
   SafeAreaView,
   ScrollView,
@@ -11,6 +12,7 @@ import {
   Text,
   useColorScheme,
   View,
+  Dimensions
 } from 'react-native';
 import {
     GoogleSignin,
@@ -20,22 +22,33 @@ import {
 
 import {googleSignout} from '../action/auth'
 import Settings from './Settings';
+GoogleSignin.configure({
+  webClientId: '350416576934-3qnqa9niinbaikun27jg1vid04kj21c1.apps.googleusercontent.com',
+});
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 
 const Home = ({googleSignout,navigation}) => {
+
+  const [searchQuery, setSearchQuery] = useState('');
+
   const _goBack = () => console.log('Went back');
 
   const _handleSearch = () => console.log('Searching');
 
   const _handleMore = () => navigation.navigate("Settings");
-    useEffect(() => {
 
-        GoogleSignin.configure({
-          webClientId: '350416576934-3qnqa9niinbaikun27jg1vid04kj21c1.apps.googleusercontent.com',
-        });
-      }, [])
-  
+  const [corosel, setcorosel] = useState(0);
 
+      const MainThemeData = [
+        {id:1, logo:'',theme:'Insights'},
+        {id:2, logo:'',theme:'COVIDCare'},
+        {id:3, logo:'',theme:'poll'},
+        {id:4, logo:'',theme:'Tech'},
+
+      ];
 
       // const signOut = async () => {
       //   console.log('Signingout');
@@ -49,32 +62,56 @@ const Home = ({googleSignout,navigation}) => {
       //     console.error(error);
       //   }
       // };
+
+      
+      const renderItem = ({item, index}) => {
+        return (
+            <View style={{height:80,width:120,backgroundColor:'gray',justifyContent:'center',borderRadius:5}}>
+                <Text style={{alignSelf:'center',alignContent:'center'}}>{ item.theme }</Text>
+            </View>
+        );
+    }
     return(
-        <View>
+        <ScrollView>
            <Appbar.Header
            style={{backgroundColor:'#EB4D4B'}}
            >
 
-      <Appbar.Content title="Title"/>
-      <Appbar.Action icon="magnify" onPress={_handleSearch} />
+      <Appbar.Content title="Discover"/>
+
       <Appbar.Action icon="cog-outline" onPress={_handleMore} />
     </Appbar.Header>
 
       <View>
-        
+        <View style={{padding:20}}>
+        <Searchbar
+      placeholder="Search"
+      onChangeText={() => {}}
+      value={searchQuery}
+    />
+        </View>
+        <View>
+
+        <Carousel
+              ref={(c) => { setcorosel(c) }}
+              data={MainThemeData}
+              renderItem={renderItem}
+              
+              sliderWidth={windowWidth}
+              itemWidth={130}
+            />
+
+
+          </View>
+
+          <View>
+          <Subheading >SUGGESTED TOPICS</Subheading >
+            
+            </View>
+      
       </View>
 
-
-
-            {/* <GoogleSigninButton onPress={() => googleSignout()}/>
-            <View>
-              <TouchableOpacity style={{backgroundColor:'blue'}}>
-                <Text style={{color:'white',padding:30}}>
-                  Phone Signout
-                </Text>
-              </TouchableOpacity>
-            </View> */}
-        </View>
+        </ScrollView>
     )
 }
 
