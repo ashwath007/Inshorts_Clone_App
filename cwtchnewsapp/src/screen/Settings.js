e1d64a512a5a19afcd1d569f54e37f4bd9ea4bc1
vicky <srivicky2000@gmail.com>
2021-06-12 16:01:53 +0530

    Depoyed brk

--- cwtchnewsapp/src/screen/Settings.js
+++ cwtchnewsapp/src/screen/Settings.js
@@ -1,5 +1,7 @@
 import React,{useEffect,useState} from 'react';
 import { StyleSheet,View,  Text } from 'react-native';
+import { Appbar,Divider } from 'react-native-paper';
+import { List } from 'react-native-paper';
 import {
     GoogleSignin,
     GoogleSigninButton,
@@ -16,6 +18,13 @@ GoogleSignin.configure({
 
 const Settings = ({userDetails}) => {
 
+
+    const _goBack = () => console.log('Went back');
+
+    const _handleSearch = () => console.log('Searching');
+  
+    const _handleMore = () => {};
+
     const [token, settoken] = useState('');
     const [userData, setuserData] = useState(userDetails);
 
@@ -27,25 +36,53 @@ const Settings = ({userDetails}) => {
     }, [])
 
 
+    const options = [
+        { oname:'Auto Start', subname: '',logo:'notification'},
+        { oname:'Language', subname: 'Choose Language',logo:'notification'},
+        { oname:'Notifications', subname: 'Please enable to receive notification',logo:'notification'},
+        { oname:'Personalize Your Feed', subname: 'Please enable to receive notification',logo:'notification'},
+        { oname:'HD Images', subname: 'Please enable to receive notification',logo:'notification'},
+        { oname:'Night Mode', subname: 'Please enable to receive notification',logo:'notification'},
+        { oname:'Autoplay', subname: 'Please enable to receive notification',logo:'notification'},
+    ]
+
+  
+
     return(
         <View>
-            <Text>
-                Settings
-            </Text>
-        {userDetails.user}
+<Appbar.Header
+           style={{backgroundColor:'#EB4D4B'}}
+           >
+
+      <Appbar.Content title="Settings"/>
+
+      <Appbar.Action icon="account-circle" onPress={_handleMore} />
+    </Appbar.Header>
+
         {console.log(userDetails)}
-            {userData ? (
-                <Text>  
-                        {userData.user}
-                </Text>
+            <View>
 
-            ) : (
-                <Text>
-                        Hooo
-                </Text>
+            <List.Section>
+            {options.map((op,index) => {
+                return(
+                    <>
+                    <List.Item
+                    title={op.oname}
+                    description={op.subname}
+                    left={props => <List.Icon {...props} icon={op.logo}/>}
+                  />
+                     <Divider />
+                  </>
+                )
+            })
 
-            )
-            }
+}
+    </List.Section>
+               
+            </View>
+
+
+
         </View>
     )
 }
