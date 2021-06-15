01100d05015028c668906af9e4dc74bdcf059cc6
vicky <srivicky2000@gmail.com>
2021-06-15 19:26:48 +0530

    Home UI done

--- cwtchnewsapp/src/screen/Home.js
+++ cwtchnewsapp/src/screen/Home.js
@@ -1,11 +1,12 @@
 import React,{useEffect,useState} from 'react';
 import { connect} from 'react-redux'
 import { Appbar,Searchbar,Subheading   } from 'react-native-paper';
-import {Image} from 'react-native'
+
 import propTypes from 'prop-types'
 import Carousel from 'react-native-snap-carousel';
 import { getCore } from '../action/core';
 import { getTopics } from '../action/topics';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import Splash from './SplashScreen/Splash';
 import { SectionGrid,FlatGrid } from 'react-native-super-grid';
 import {
@@ -16,6 +17,7 @@ import {
   TouchableOpacity,
   FlatList,
   Text,
+  Image,
   useColorScheme,
   View,
   Dimensions
@@ -57,39 +59,16 @@ const Home = ({getCore,getTopics,topicState,coreState,googleSignout,navigation})
       ];
 
       const MainCategoryData = [
-        {id:1,logo:'',topic:'MY FEED'},
-        {id:2,logo:'',topic:'ALL NEWS'},
-        {id:3,logo:'',topic:'TOP STORIES'},
-        {id:4,logo:'',topic:'TRENDING'},
-        {id:5,logo:'',topic:'BOOKMARKS'},
-        {id:6,logo:'',topic:'UNREAD'},
+        {id:1,icon:'feed',topic:'MY FEED'},
+        {id:2,icon:'globe',topic:'ALL NEWS'},
+        {id:3,icon:'bar-chart',topic:'TOP STORIES'},
+        {id:4,icon:'magic',topic:'TRENDING'},
+        {id:5,icon:'bookmark',topic:'BOOKMARKS'},
+        {id:6,icon:'bars',topic:'UNREAD'},
 
 
       ];
 
-
-      const [items, setItems] = useState([
-        { name: 'TURQUOISE', code: '#1abc9c' },
-        { name: 'EMERALD', code: '#2ecc71' },
-        { name: 'PETER RIVER', code: '#3498db' },
-        { name: 'AMETHYST', code: '#9b59b6' },
-        { name: 'WET ASPHALT', code: '#34495e' },
-        { name: 'GREEN SEA', code: '#16a085' },
-        { name: 'NEPHRITIS', code: '#27ae60' },
-        { name: 'BELIZE HOLE', code: '#2980b9' },
-        { name: 'WISTERIA', code: '#8e44ad' },
-        { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
-        { name: 'SUN FLOWER', code: '#f1c40f' },
-        { name: 'CARROT', code: '#e67e22' },
-        { name: 'ALIZARIN', code: '#e74c3c' },
-        { name: 'CLOUDS', code: '#ecf0f1' },
-        { name: 'CONCRETE', code: '#95a5a6' },
-        { name: 'ORANGE', code: '#f39c12' },
-        { name: 'PUMPKIN', code: '#d35400' },
-        { name: 'POMEGRANATE', code: '#c0392b' },
-        { name: 'SILVER', code: '#bdc3c7' },
-        { name: 'ASBESTOS', code: '#7f8c8d' },
-      ]);
       // const signOut = async () => {
       //   console.log('Signingout');
 
@@ -119,16 +98,28 @@ const Home = ({getCore,getTopics,topicState,coreState,googleSignout,navigation})
       
       const renderItem = ({item, index}) => {
         return (
-            <View key={index} style={{height:100,width:190,backgroundColor:'gray',justifyContent:'center',borderRadius:5}}>
-                <Text style={{alignSelf:'center',alignContent:'center'}}>{ item.title }</Text>
+            <View key={index} style={{height:100,width:190,backgroundColor:item.color,borderRadius:5}}>
+                  <Image
+              source={{uri:item.logo}}
+              style={{height:60,width:60,alignSelf:'center',justifyContent:'center',marginTop:22}}
+          />  
+                {/* <Text style={styles.itemName,{alignSelf:'center'}}>{ item.title }</Text>   */}
             </View>
         );
     }
 
     const renderCategory = ({item, index}) => {
       return(
-        <View style={{height:100, width:100, backgroundColor:'gray',marginRight:20,borderRadius:8}}>
-              <Text style={{alignSelf:'center',justifyContent:'flex-start'}}>
+        <View style={{height:100, width:100, backgroundColor:'white',marginRight:20,borderRadius:8}}>
+              <Icon
+              style={{alignSelf:'center',fontSize:30,justifyContent:'center',padding:10}}
+              
+  reverse
+  name={item.icon}
+  type='ionicon'
+  color='#000'
+/>
+              <Text style={{alignSelf:'center'}}>
                 {item.topic}
               </Text>
         </View>
